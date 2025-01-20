5f1820e6ad5a7eb7f1d62500d6888864f6f3b56a
Anantayaputt <s42895@bj.ac.th>
2025-01-20 15:46:31 +0700

    Add files via upload

---
+++ a4_slicer_37/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1737362715,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/botcut-sheet0.png",
+		"images/botcut-sheet1.png",
+		"images/botcut-sheet2.png",
+		"images/grape_bot-sheet0.png",
+		"images/strawberry-sheet0.png",
+		"images/strawberry-sheet1.png",
+		"images/grape_w2-sheet0.png",
+		"images/spawn-sheet0.png",
+		"images/fruitslash-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
